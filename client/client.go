68a8fd3ecc68c3f5d0308fc65e98cd75c72c9283
Santatra <razafindratsimba34@gmail.com>
2024-10-26 00:19:13 +0300

    add API client health checking

--- client/client.go
+++ client/client.go
@@ -7,11 +7,24 @@ import (
 )
 
 const baseUrl = "https://motmalgache.org/bins"
+var homepageEnd = fmt.Sprintf("%s/homePage", baseUrl)
 var searchEndpoint = fmt.Sprintf("%s/teny2", baseUrl)
 
-func Search(keyword string) {
+func Health() int {
+    resp, err := http.Get(homepageEnd)
+    var resCode int;
+    if resp.StatusCode > 299 || err != nil {
+	resCode = 1
+    } else {
+	resCode = 0
+    }
+    defer resp.Body.Close()
+    return resCode
+}
+
+func Search(keyword string) string {
     resp, _ := http.Get(searchEndpoint)
     defer resp.Body.Close()
     respBody, _ := io.ReadAll(resp.Body)
-    fmt.Println(string(respBody))
+    return string(respBody)
 }
