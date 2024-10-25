94f37e0e748e1ae8c94bd7c0da4f48f967621d18
Harimbola Santatra <razafindratsimba34@gmail.com>
2024-10-25 23:36:12 +0300

    Feat: HTML parser and more (#7)
    * add requirements.txt
    
    * refac: create a Document class
    
    * implement get_entry_word: extract the keyword provided by the user
    
    * refac: fix indentation of result.html
    
    * doc: add instruction to setup python
    
    * doc: more instruction for venv
    
    * add module: 'rich'
    
    * remove <span class="minute">
    
    * more error handling

--- readme.md
+++ readme.md
@@ -3,6 +3,15 @@ A cli client for [motmalgache.org][1].
 
 [motmalgache.org][1] is an alternative version of [mondemalgache.org](mondemalgache.org).
 
+## Setup
+Setup a python virtual environment:
+```
+mkdir ~/.python-venv
+python3 -m venv ~/.python-venv/bs4
+source ~/.python-venv/bs4/bin/activate
+python3 -m pip install -r requirements.txt
+```
+
 ## Usage
 List of commands:
 - `search`: search for word
--- scraping-test/scraping.py
+++ scraping-test/scraping.py
@@ -1,24 +1,135 @@
-from bs4 import BeautifulSoup
-import os
-import sys
+import os, sys
+import typing
+
+def print_module_error(module_name):
+    print(f"{module_name} is required! Maybe you forget to enable a virtual environment!")
+
+try:
+    import bs4
+except ModuleNotFoundError:
+    print_module_error("bs4")
+    sys.exit(1)
+
+
+try:
+    from rich.console import Console
+except ModuleNotFoundError:
+    print_module_error("rich")
+    sys.exit(1)
+
 
 # Search for a tag who has a class 'class_name'
-def get_class(tag, class_name):
-    return tag.find_all("span", class_=class_name)
+def get_class(tag, class_name) -> bs4.Tag :
+    return tag.find("span", class_=class_name)
+
+class Document:
+    """
+    Represent a HTML document in response to a query from motmalagasy.org
+    """
+
+    def __init__(self, filename):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        filename: string
+            get the name of the HTML file
+        """
+        # open, read and close file
+        fp = open(filename, "r")
+        soup = bs4.BeautifulSoup(fp, 'html.parser')
+        fp.close()
+
+        # self.soup is a <html> tag object
+        self.soup = soup
+
+        # 'rich' module for pretty printing
+        self.console = Console()
+
+
+    def get_entry_word(self) -> str:
+        """
+        Get the keywords entered by the user
+        In french: Entrée
+        """
+        # find the 'entryWord' class
+        entry_word_tag: bs4.Tag = get_class(self.soup, "entryWord")
+        # the result of the above expression is always in the format:
+        # <span class="entryWord">lako<u>zi</u>a<a class="entryWord" name="mg.n"> </a></span>
+        # ... so we need to flatten it
+        entry_word = ""
+        for child in entry_word_tag.children:
+            # append to result if 'child' is a NavigableString
+            entry_word += child if type(child) == bs4.element.NavigableString else child.string
+        return entry_word
+
+
+    def get_result_pairs(self) -> list:
+        """
+        Get all the pair of result.
+        e.g:
+            Entrée: lakozia
+            Partie du discours: nom
+            Vocabulaire Economie: alimentation
+
+        We need to get a table tag, which some particular attribute:
+            - name="td"
+            - width="20%"
+            - align="right"
+
+        Returns
+        -------
+        list
+            Each row is a key-value pair:
+                {
+                    "label": "Explication en malgache",
+                    "text": "Trano fanaovana nahandro"
+                }
+        """
+        tables: list = self.soup.find_all(name="td", width="20%", align="right")
+        result = []
+
+        for table in tables:
+            # table[4:] because we do not want to print the first 4 <table>,
+            # which is the navigation menu and some unknown BS!
+            # TODO: this needs a hard edit cuz that guy was fkn lazy!
+
+            new_entry = {}
+            # take a look at the html file (result.html) to understand this section
+            new_entry["label"] = table.string
+
+            # remove <span class="minute">
+            table.next_sibling.next_sibling.find("span", class_="minute").extract()
+
+            # put next_sibling 2 times because look at https://www.crummy.com/software/BeautifulSoup/bs4/doc/#next-sibling-and-previous-sibling
+            new_entry["text"]  = "".join(table.next_sibling.next_sibling.stripped_strings)
+
+            result.append(new_entry)
+
+        return result
+
+    def print_table(self):
+        """
+        Pretty print the result of get_result_pairs()
+        """
+        tables = self.get_result_pairs()
+        for table in tables:
+            self.console.print(table["label"], end="", style="green")
+            print(": {}".format(table["text"]))
+
 
 def main():
+
     html_filename = "result.html"
 
     # get path of the executable
     file_path = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(file_path, html_filename)
 
-    # open, read and close file
-    fp = open(os.path.join(file_path, html_filename), "r")
-    soup = BeautifulSoup(fp, 'html.parser')
-    fp.close()
+    doc = Document(filename)
+    doc.print_table()
 
-    entry_word = get_class(soup, "entryWord")
-    print(entry_word)
 
 if __name__ == '__main__':
     main()
