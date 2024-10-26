8fc89944b0c09e6cecd4b89ec2c745bfd8e551a0
Santatra <razafindratsimba34@gmail.com>
2024-10-26 20:52:31 +0300

    wip: url form data cannot be sent to server

--- client/client.go
+++ client/client.go
@@ -1,10 +1,13 @@
 package client
 
 import (
-    "net/http"
-    "io"
     "fmt"
+    "io"
     "log"
+    "net/http"
+    "net/url"
+    "strings"
+    "time"
 )
 
 const baseUrl = "https://motmalgache.org/bins"
@@ -23,22 +26,60 @@ func Health() int {
     return resCode
 }
 
-// Data for search
+// request Data
 type Data struct {
-    keyword string
+    requestData string
 }
 
 func (data Data) Read(p []byte) (n int, err error) {
     return len(p), err
 }
 
-func Search(keyword string) string {
-    data := Data{ keyword }
-    resp, err := http.Post(searchEndpoint, "text/html", data)
-    defer resp.Body.Close()
-    if err != nil {
-	log.Fatal("Error in posting to motmalgache!")
+func Search(keyword string) {
+    // Create Client
+    client := &http.Client{
+	Timeout: time.Second * 15,
     }
-    respBody, _ := io.ReadAll(resp.Body)
-    return string(respBody)
+
+    // Set request data
+    data := url.Values{}
+    data.Add("w", keyword)
+    encodedData := data.Encode()
+
+
+    // Create Request
+    req, err := http.NewRequest("post", searchEndpoint, strings.NewReader(encodedData))
+    if err != nil {
+	log.Fatalf("Error on creating a 'Request': %s", err.Error())
+    }
+
+    parseErr := req.ParseForm()
+    if parseErr != nil {
+	log.Fatalf("Error on parsing form: %s", err.Error())
+    }
+
+    // Set request header
+    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+    // Make request
+    resp, err := client.Do(req)
+    if err != nil {
+	log.Fatalf("Request Error: %s", err.Error())
+    }
+
+    // body, _ := io.ReadAll(resp.Body)
+    // fmt.Println(string(body))
+
+    // DEBUG
+    // print the form value of the request
+
+    bd := req.Form.Get("x")
+    fmt.Println(string(bd))
+    er, _ := io.ReadAll(strings.NewReader(encodedData))
+    fmt.Println(string(er))
+    he, _ := req.Header["Content-Type"]
+    fmt.Println(he)
+
+    // Close
+    defer resp.Body.Close()
 }
