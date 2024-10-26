686fd3439db0d4e697881b7ad0e384ba88ab7b25
Santatra <razafindratsimba34@gmail.com>
2024-10-26 14:06:59 +0300

    fix `test-request.sh`: change data encoding

--- test-request.sh
+++ test-request.sh
@@ -2,7 +2,9 @@
 
 BASE_URL="https://motmalgache.org/bins"
 SEARCH_ENDPOINT="${BASE_URL}/teny2"
-curl -Lv \
+curl -Ls \
     -XPOST \
-    --data '{"w": "lakozia"}' \
+    --data 'w=lakozia' \
+    -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8", \
+    -H "Content-Type: application/x-www-form-urlencoded", \
     $SEARCH_ENDPOINT
