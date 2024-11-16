3f16603c027455675ad0d9d2b010a28e99e54ae4
Santatra <razafindratsimba34@gmail.com>
2024-11-16 17:12:07 +0300

    Refac: improve test/website-change.sh

--- test/website-change.sh
+++ test/website-change.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-temp1="$(mktemp -p test/)"
-temp2="$(mktemp -p test/)"
+# Test if the current structure of the HTML page is the same as the assumed HTML page (scraping-test/result.html)
 
-cat scraping-test/result.html | xargs -0 ./scraping > $temp1
-./test/test-request.sh | xargs -0 ./scraping > $temp2
+dir=$(basename $(pwd))
+if [ "$dir" = "test" ]; then
+    test_dir="."
+    rel_dir=".."
+else
+    test_dir="test"
+    rel_dir="."
+fi
+
+temp1="$(mktemp -p $test_dir --suffix=ASSUMED)"
+temp2="$(mktemp -p $test_dir --suffix=CURRENT)"
+
+# Assumed version
+cat ${rel_dir}/scraping-test/result.html | xargs -0 ${rel_dir}/scraping > $temp1
+
+# Current version
+${rel_dir}/test/test-request.sh | xargs -0 ${rel_dir}/scraping > $temp2
+
+# beautify temp files
+python3 -m json.tool $temp1 $temp1
+python3 -m json.tool $temp2 $temp2
 
 cmp -s $temp2 $temp1
