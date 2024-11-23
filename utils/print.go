7b0c59546e0d407815d1e21381897c1dfd9acbd6
Santatra <razafindratsimba34@gmail.com>
2024-11-23 11:51:03 +0300

    Add banner and refac 'version' variable

--- utils/print.go
+++ utils/print.go
@@ -8,26 +8,40 @@ import (
 var Reset = "\033[0m" 
 var Red = "\033[31m" 
 var Green = "\033[32m" 
-var Yellow = "\033[33m" 
-var Blue = "\033[34m" 
-var Magenta = "\033[35m" 
-var Cyan = "\033[36m" 
-var Gray = "\033[37m" 
 var White = "\033[97m"
 
 const banner = `
-
- #    #  ####  #    # #####  ###### #    #   ##   #       ####    ##    ####  #    # ######      ####  #####   ####  
- ##  ## #    # ##   # #    # #      ##  ##  #  #  #      #    #  #  #  #    # #    # #          #    # #    # #    # 
- # ## # #    # # #  # #    # #####  # ## # #    # #      #      #    # #      ###### #####      #    # #    # #      
- #    # #    # #  # # #    # #      #    # ###### #      #  ### ###### #      #    # #      ### #    # #####  #  ### 
- #    # #    # #   ## #    # #      #    # #    # #      #    # #    # #    # #    # #      ### #    # #   #  #    # 
- #    #  ####  #    # #####  ###### #    # #    # ######  ####  #    #  ####  #    # ###### ###  ####  #    #  ####  
+ #    # #    #        ####  #      # 
+ ##  ## ##  ##       #    # #      # 
+ # ## # # ## # ##### #      #      # 
+ #    # #    #       #      #      # 
+ #    # #    #       #    # #      # 
+ #    # #    #        ####  ###### # 
 
 `
+const ruler = "===================================="
+
+const mmContributorLink = "https://motmalgache.org/bins/contributors"
+const mmHomepage = "https://motmalgache.org/bins/homePage"
+const sourceCodeLink = "https://github.com/HarimbolaSantatra/mm-cli"
+
+func GetVersion() string {
+    version := "0.1b"
+    return version
+}
+
+func PrintRuler() {
+    fmt.Println("\n" + ruler + "\n")
+}
 
 func PrintBanner() {
+    PrintRuler()
     fmt.Printf("%s", banner)
+    fmt.Println(Red, "Monde Malgache homepage:", Reset , mmHomepage)
+    fmt.Println(Red, "Monde Malgache contributors:", Reset, mmContributorLink)
+    fmt.Println(Red, "Source code:", Reset, sourceCodeLink)
+    fmt.Println(Red, "Version:", Reset, GetVersion())
+    PrintRuler()
 }
 
 func PrintLineTitle(title string, content string) {
