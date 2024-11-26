a2de54b7fc8641889e6ab2fbfee0fe698ea0e1dd
Harimbola Santatra <razafindratsimba34@gmail.com>
2024-11-26 20:41:05 +0300

    Handle python missing packages in go (#21)
    * Handle python missing packages in go
    
    * test.py: close file handle and remove uncessarry pipe
    
    * Improve makefile
    
    - add install and uninstall target
    - add more PHONY
    
    * Handle python missing packages in go

--- client/parser.go
+++ client/parser.go
@@ -7,6 +7,12 @@ import (
     "path/filepath"
 )
 
+// Handle error related to Python requirements
+func LogPythonError(module string) {
+    log.Fatalf("Python module \"%s\" not found! Create a virtual environment and install python packages with 'pip install -r requirements.txt'", module)
+}
+
+
 // Get a HTML formatted string and return the JSON format
 // This function uses the python script `./mm-scraping` to do that.
 // Input: a string in HTML format
@@ -23,7 +29,12 @@ func ParseString(s string) string {
     cmd := exec.Command(exPath, s)
     bt, err := cmd.Output()
     if err != nil {
-	log.Fatalf("Command execution error: %s", err)
+	if err.Error() == "exit status 2" {
+	    LogPythonError("bs4")
+	}
+	if err.Error() == "exit status 3" {
+	    LogPythonError("rich")
+	}
     }
     return string(bt)
 }
