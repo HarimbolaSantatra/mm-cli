c22af8cbd811bce8e8a89c82c27315def6abae8b
Santatra <razafindratsimba34@gmail.com>
2024-12-21 21:17:53 +0300

    fix go test `utils/`

--- utils/print_test.go
+++ utils/print_test.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+    "fmt"
     "strings"
     "testing"
 )
+
+// Function used by test function to print debugging information
+func DebugPrint(actual, expected string) string {
+    return fmt.Sprintf( "Got: \"%s\"\nExpecting: \"%s\"", actual, expected )
+}
 
 
 func TestClean(t *testing.T) {
@@ -11,6 +17,16 @@ func TestClean(t *testing.T) {
     y := Clean(x)
     expected := "Ito dia texte fanao testmande tsara v?]"
     if strings.Compare(y, expected) != 0 {
-	t.Errorf("Got: \"%s\"\nExpecting: \"%s\"", y, expected)
+	t.Errorf(DebugPrint(y, expected))
+    }
+}
+
+func TestPrintKeyAndValue(t *testing.T) {
+    const title = "Soratra maintso"
+    const content = "soratra fotsy"
+    expected := fmt.Sprintf("\t%s- %s: %s%s", Green, title, Reset, content)
+    x := PrintKeyAndValue(title, content, false)
+    if strings.Compare(x, expected) != 0 {
+	t.Errorf("Got: \"%s\"\nExpecting: \"%s\"", x, expected)
     }
 }
